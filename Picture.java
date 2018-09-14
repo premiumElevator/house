b06ca1259aa91a0a57fb1626b9b51bf9427fc992
premiumElevator <43154659+premiumElevator@users.noreply.github.com>
2018-09-14 16:27:55 -0400

    drew background

--- Picture.java
+++ Picture.java
@@ -11,9 +11,9 @@
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
+    private Square background;
+    private Square background2;
+    private Triangle hill;
     private Circle sun;
     private boolean drawn;
 
@@ -22,9 +22,9 @@ public class Picture
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
+        background = new Square();
+        background2 = new Square();
+        hill = new Triangle();  
         sun = new Circle();
         drawn = false;
     }
@@ -35,21 +35,25 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
+            background2.changeColor("blue");
+            background2.moveHorizontal(-375);
+            background2.moveVertical(-180);
+            background2.changeSize(580);
+            background2.makeVisible();
+            
+            background.changeColor("green");
+            background.moveHorizontal(-375);
+            background.moveVertical(20);
+            background.changeSize(580);
+            background.makeVisible();
+            
+          
     
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
+            hill.changeSize(60, 180);
+            hill.moveHorizontal(-30);
+            hill.moveVertical(-60);
+            hill.makeVisible();
     
             sun.changeColor("yellow");
             sun.moveHorizontal(100);
@@ -65,9 +69,9 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
+        background.changeColor("black");
+        background2.changeColor("white");
+        hill.changeColor("black");
         sun.changeColor("black");
     }
 
@@ -76,9 +80,9 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
+        background.changeColor("red");
+        background2.changeColor("black");
+        hill.changeColor("green");
         sun.changeColor("yellow");
     }
 }
